18f2dc695e3e9d98e8e13c723ed6dd506190a9f7
Victor <vchinoesp@gmail.com>
2025-12-22 21:00:55 +0100

    formulario 22/12 21:00

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                form: resolve(__dirname, 'form.html'),
+            }
+        }
+    }
+});
